e51303349a20faf8b1db1cd42e2f832063062026
shabeersf <shabeermen@gmail.com>
2026-01-19 09:17:19 +0530

    first commit

--- app/api/mobile-api/auth/security-login/route.js
+++ app/api/mobile-api/auth/security-login/route.js
@@ -53,7 +53,7 @@ export async function POST(request) {
         { status: 401 }
       );
     }
-
+// test
     // Verify password
     const isValidPassword = await bcrypt.compare(password, security.password);
     
